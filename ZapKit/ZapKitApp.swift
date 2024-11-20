d9214a45ee3adfe38d4562aadee8e4a261a4e7e5
Zigao Wang <zigao.wang24@gmail.com>
2024-11-20 16:09:59 +0800

    refractor: initial commit

---
+++ ZapKit/ZapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZapKitApp.swift
+//  ZapKit
+//
+//  Created by Zigao Wang on 11/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ZapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
